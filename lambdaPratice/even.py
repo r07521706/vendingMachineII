04ad265995c89ec066ed64710cf384f2c98d878e
kolin <r07521706@g.ntu.edu.tw>
2020-06-20 23:49:02 +0800

    cool

--- lambdaPratice/even.py
+++ lambdaPratice/even.py
@@ -3,16 +3,32 @@ import json
 def lambda_handler(event, context):
     number = event['queryStringParameters']['number']
     ans = "no"
-    if(int(number)%2==0):
+
+    if(type(number)==str):
+        ans = "str~~"
+        ## body
+        ansResp = {}
+        ansResp['result'] = ans
+        # ## resp body
+        respObj = {}
+        respObj['statusCode'] = 500
+        respObj['headers']={}
+        respObj['headers']['Content-Type'] = 'application/json'
+        respObj['body']=json.dumps(ansResp)
+        # TODO implement
+    
+    elif(int(number)%2==0):
         ans = "yes"
-    ## body
-    ansResp = {}
-    ansResp['result'] = ans
-    # ## resp body
-    respObj = {}
-    respObj['statusCode'] = 200
-    respObj['headers']={}
-    respObj['headers']['Content-Type'] = 'application/json'
-    respObj['body']=json.dumps(ansResp)
-    # TODO implement
+        ## body
+        ansResp = {}
+        ansResp['result'] = ans
+        # ## resp body
+        respObj = {}
+        respObj['statusCode'] = 200
+        respObj['headers']={}
+        respObj['headers']['Content-Type'] = 'application/json'
+        respObj['body']=json.dumps(ansResp)
+        # TODO implement
+    
+    
     return respObj
